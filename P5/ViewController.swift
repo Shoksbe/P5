55be44c683bc83f14b8b031adcb05d86d59f63ec
Shoksbe <deknyfgregory@gmail.com>
2018-04-30 17:34:25 +0200

    Initial Commit

---
+++ P5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P5
+//
+//  Created by De knyf Gregory on 30/04/18.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
