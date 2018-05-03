f1e5dfd4c5630d2a31f0d33e5c041bbb59da5987
Shoksbe <deknyfgregory@gmail.com>
2018-05-03 15:08:51 +0200

    feat(ViewController): connect the view with the controller
    The user can now choose the layout via the appropriate button
    and it will be displayed in the main layout.

--- P5/Controller/ViewController.swift
+++ P5/Controller/ViewController.swift
@@ -12,14 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setLayout(2)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //This view contains layouts for photos
+    @IBOutlet weak var mainLayoutView: MainLayoutView!
+    
+    @IBAction func didTapLayoutButton(_ sender: UIButton) {
+        setLayout(sender.tag)
     }
-
-
+    
+    //Activates the layout chooses via the button
+    private func setLayout(_ selectedButtonTag: Int) {
+        //get the button tag to identify the layout
+        switch selectedButtonTag {
+        case 1: // BigTop, BottomLeft, BottomRight
+            mainLayoutView.layout = .oneTopAndTwoBottom
+        case 2: // BigBottom, TopLeft, TopRight
+            mainLayoutView.layout = .twoTopAndOneBottom
+        case 3: // TopLeft, topRight, BottomLeft, BottomRight
+            mainLayoutView.layout = .twoTopAndTwoBottom
+        default:
+            break
+        }
+    }
 }
 
