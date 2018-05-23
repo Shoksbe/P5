18abe101c25d07f05f948f28743397b966005f1b
Shoksbe <deknyfgregory@gmail.com>
2018-05-23 14:51:03 +0200

    refact: remove gesture from controller and adding in storyboard. Add a completion for sharing layout.

--- P5/Controller/ViewController.swift
+++ P5/Controller/ViewController.swift
@@ -26,44 +26,34 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         //Default layout
         mainLayoutView.layout = .twoTopAndOneBottom
-        
-        //
-        //GESTURE FOR CHOOSE PICTURE
-        //
-        
-        //Browse all the views that can receive a photo in mainViewLayout to add the gestures.
-        for view in mainLayoutView.ImageView {
-            
-            //GestureRecognizer to be able to click on the layout where the user wants to add a photo
-            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(setImage(_:)))
-            
-            //Enabling user interaction on the layout
-            view.isUserInteractionEnabled = true
-            
-            //Add gesture's layout
-            view.addGestureRecognizer(tapGestureRecognizer)
-        }
-        
-        //
-        //GESTURE FOR SHARE INSTAGRID
-        //
-        
-        //GestureRecognizer to share the photo
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragLayout(_:)))
-        //add gesture's layout
-        mainLayoutView.addGestureRecognizer(panGestureRecognizer)
-        
+
+        layout = LayoutSettings(layout: mainLayoutView)
     }
 
     //This view contains layouts for photos
     @IBOutlet weak var mainLayoutView: MainLayoutView!
     
     @IBAction func didTapLayoutButton(_ sender: UIButton) {
-        setLayout(sender.tag)
+        changeLayout(sender.tag)
+    }
+    @IBAction func didTapLayout(_ sender: UITapGestureRecognizer) {
+        addImage(in: sender)
+    }
+    @IBAction func dragLayout(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began, .changed:
+            transformLayoutViewWith(gesture: sender)
+        case .cancelled, .ended:
+            transformLayoutToIdentity {
+                shareLayout()
+            }
+        default:
+            break
+        }
     }
     
     //Activates the layout chooses via the button
-    private func setLayout(_ selectedButtonTag: Int) {
+    private func changeLayout(_ selectedButtonTag: Int) {
         //get the button tag to identify the layout
         switch selectedButtonTag {
         case 1: // BigTop, BottomLeft, BottomRight
@@ -76,53 +66,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             break
         }
     }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
-        //Save image choosen by the user
-        let choosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage
-        
-        //Some image's option
-        currentImageView.clipsToBounds = true
-        currentImageView.contentMode = .scaleAspectFill
-        
-        //Affect image choosen by the user on the layout
-        currentImageView.image = choosenImage
-        
-        //Close pickerController
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    @objc func setImage(_ sender: UITapGestureRecognizer) {
-        
-        //save the selected layout to put the chosen photo
-        currentImageView = sender.view as! UIImageView
-        
-        //
-        //PickerController settings
-        //
-        let imageController = UIImagePickerController()
-        imageController.delegate = self
-        
-        //Selection of the source of photos, here it will be the library but it can also be the camera.
-        imageController.sourceType = .photoLibrary
-        
-        //present the pickerController for choos a image
-        self.present(imageController, animated: true, completion: nil)
-    }
-    
-    @objc func dragLayout(_ sender: UIPanGestureRecognizer) {
-        switch sender.state {
-        case .began, .changed:
-            transformLayoutViewWith(gesture: sender)
-        case .cancelled, .ended:
-            transformLayoutToIdentity()
-            shareLayout()
-        default:
-            break
-        }
-    }
-    
+
     private func transformLayoutViewWith(gesture: UIPanGestureRecognizer) {
 
         //Get the finger position on the screen
@@ -145,23 +89,57 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     private func shareLayout() {
-
         //Transformation of UIView into UIImage
-        let imageToshare = mainLayoutView.asImage()
-
-        //Creation and display of the UIActivityController
-        let activityVC = UIActivityViewController(activityItems: [imageToshare], applicationActivities: [])
-        activityVC.popoverPresentationController?.sourceView = self.view
-        self.present(activityVC, animated: true, completion: nil)
+        if let imageToShare = layout.toImage() {
+            //Creation and display of the UIActivityController
+            let activityVC = UIActivityViewController(activityItems: [imageToShare], applicationActivities: [])
+            activityVC.popoverPresentationController?.sourceView = self.view
+            self.present(activityVC, animated: true, completion: nil)
+        }
     }
     
-    private func transformLayoutToIdentity() {
+    private func transformLayoutToIdentity(completion: () -> ()) {
         UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
             self.mainLayoutView.transform = .identity
         }, completion:nil)
+        completion()
     }
-    
-    struct DeviceInfo {
+
+    private func addImage(in sender: UITapGestureRecognizer) {
+
+        //save the selected layout to put the chosen photo
+        currentImageView = sender.view as! UIImageView
+
+        //
+        //PickerController settings
+        //
+        let imageController = UIImagePickerController()
+        imageController.delegate = self
+
+        //Selection of the source of photos, here it will be the library but it can also be the camera.
+        imageController.sourceType = .photoLibrary
+
+        //present the pickerController for choos a image
+        self.present(imageController, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+        //Save image choosen by the user
+        let choosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+
+        //Some image's option
+        currentImageView.clipsToBounds = true
+        currentImageView.contentMode = .scaleAspectFill
+
+        //Affect image choosen by the user on the layout
+        currentImageView.image = choosenImage
+
+        //Close pickerController
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    private struct DeviceInfo {
         struct Orientation {
             // indicate current device is in the LandScape orientation
             static var isLandscape: Bool {
