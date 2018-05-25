3e6334989fc75910b2777df5cae0ed55413fb4ce
Shoksbe <deknyfgregory@gmail.com>
2018-05-25 15:54:00 +0200

    feat(): addition of the class "TransformView" which contains the function "toImage"

---
+++ P5/Model/TransformView.swift
@@ -0,0 +1,24 @@
+//
+//  Layout.swift
+//  P5
+//
+//  Created by De knyf Gregory on 21/05/18.
+//  Copyright © 2018 De knyf Gregory. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TransformView{
+
+    static func toImage(mainLayout: UIView) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(mainLayout.bounds.size, mainLayout.isOpaque, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        if let context = UIGraphicsGetCurrentContext() {
+            mainLayout.layer.render(in: context)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            return image
+        }
+        return nil
+    }
+}
